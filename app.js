9c68577462b0a9804b84d134d7e49864117f3cf6
myk245 <myk245@nyu.edu>
2020-08-26 22:52:57 -0400

    constructor function and this keyword

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+// Object Literal
+// const cindy = {
+//    name: 'Cindy', 
+//    age: '28'
+// }
+
 // Person constructor
-function Person() {
-   this.name = 'Cindy'; 
+function Person(name, dob) {
+   this.name = name;
+   // this.age = age; 
+   this.birthday = new Date(dob); 
+   this.calculateAge = function () {
+      const diff = Date.now() - this.birthday.getTime();
+      const ageDate = new Date(diff);
+      return Math.abs(ageDate.getUTCFullYear() - 1970); 
+   }
 }
 
-const cindy = new Person(); 
+// const cindy = new Person('Cindy', 28); 
+// const david = new Person('David', 29); 
 
-console.log(cindy); 
+const cindy = new Person('Cindy', '3/10/1992'); 
+console.log(cindy.calculateAge());
