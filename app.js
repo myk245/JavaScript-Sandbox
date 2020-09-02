ad5c6adb0c7a10d17d173b1442371caf06c78a40
myk245 <myk245@nyu.edu>
2020-09-01 23:36:49 -0400

    prototypal inheritance

--- app.js
+++ app.js
@@ -8,8 +8,7 @@
 function Person(firstName, lastName, dob) {
    this.firstName = firstName;
    this.lastName = lastName; 
-   // this.age = age; 
-   this.birthday = new Date(dob); 
+   // this.birthday = new Date(dob); 
    // this.calculateAge = function () {
    //    const diff = Date.now() - this.birthday.getTime();
    //    const ageDate = new Date(diff);
@@ -34,15 +33,33 @@ Person.prototype.getsMarried = function(newLastName) {
    this.lastName = newLastName; 
 }
 
+// Greeting
+Person.prototype.greeting = function() {
+   return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
 const cindy = new Person('Cindy', 'Kei', '03/10/1992'); 
 const david = new Person('David', 'Messer', '01/03/1991')
 
-console.log(cindy);
-console.log(david.calculateAge());
-console.log(cindy.getFullName()); 
+// console.log(cindy);
+// console.log(david.calculateAge());
+// console.log(cindy.getFullName()); 
 
-cindy.getsMarried('Evans'); 
-console.log(cindy.getFullName());  
+// cindy.getsMarried('Evans'); 
+// console.log(cindy.getFullName());  
 
-console.log(cindy.hasOwnProperty('firstName')); // true
-console.log(cindy.hasOwnProperty('getFullName')); // false
+// console.log(cindy.hasOwnProperty('firstName')); // true
+// console.log(cindy.hasOwnProperty('getFullName')); // false
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+   Person.call(this, firstName, lastName);
+   
+   this.phone = phone; 
+   this.membership = membership; 
+}
+
+// Create customer 
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard'); 
+
+console.log(customer1); 
