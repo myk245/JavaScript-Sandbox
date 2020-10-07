35ca837ba1a79c7a3a921d1a942b7e8eb335a16f
myk245 <myk245@nyu.edu>
2020-10-07 18:50:08 -0400

    fetch call with async await

--- async-await.js
+++ async-await.js
@@ -1,18 +1,31 @@
-async function myFunc() {
- const promise = new Promise((resolve, reject) => {
-      setTimeout(() => resolve('Hello'), 1000)
-   })
+// async function myFunc() {
+//  const promise = new Promise((resolve, reject) => {
+//       setTimeout(() => resolve('Hello'), 1000)
+//    })
 
-   const error = false; 
+//    const error = false; 
 
-   if (!error) {
-      const res = await promise; // wait until promise is resolved 
-      return res; 
-   } else {
-      await Promise.reject(new Error('Something went wrong.'))
-   }
+//    if (!error) {
+//       const res = await promise; // wait until promise is resolved 
+//       return res; 
+//    } else {
+//       await Promise.reject(new Error('Something went wrong.'))
+//    }
+// }
+
+// myFunc()
+//    .then(res => console.log(res))
+//    .catch(err => console.log(err))
+
+async function getUsers() {
+   // await response of the fetch call
+   const response = await fetch('https://jsonplaceholder.typicode.com/users'); 
+
+   // only proceed once the promise is resolved
+   const data = await response.json(); 
+
+   // only proceed the second promise is resolved
+   return data; 
 }
 
-myFunc()
-   .then(res => console.log(res))
-   .catch(err => console.log(err))
+getUsers().then(users => console.log(users)); 
